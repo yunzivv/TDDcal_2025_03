66d6bdaecb1dee85611936becce49087e1378b89
yunzivv <yunzivv@gmail.com>
2025-03-18 14:14:57 +0900

    다항식 계산기

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -4,17 +4,38 @@ public class Calc {
 
     public static int run(String exp) {
 
-        String[] bits = exp.split(" "); // + 기호 사용하려면
+        String[] bits = exp.split(" "); // + 기호 사용하려면 이스케이프
+
+        boolean needToPlus = exp.equals("+");
+        boolean needToMinus = exp.equals("-");
+        boolean needToMultiply = exp.equals("*");
+        boolean needToDivide = exp.equals("/");
+
+        if (needToPlus) {
+            bits = exp.split(" \\+ ");
+        } else if (needToMinus) {
+            bits = exp.split(" - ");
+        } else if (needToMultiply) {
+            bits = exp.split(" * ");
+        } else if (needToDivide) {
+            bits = exp.split(" / ");
+        }
+
 
         int a = Integer.parseInt(bits[0]);
-        String oper = bits[1];
         int b = Integer.parseInt(bits[2]);
 
-        if (oper.equals("+")) {
+        if (needToPlus) {
             return a + b;
-        } else {
+        } else if (needToMinus) {
             return a - b;
+        } else if (needToMultiply) {
+            return a * b;
+        } else if (needToDivide) {
+            return a / b;
         }
+
+        throw new RuntimeException("해석 불가");
 
     }
 
